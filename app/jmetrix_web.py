b725a70ac7befae830ab31b9b35d88c20056003d
idimitrakopoulos <idimitrakopoulos@gmail.com>
2023-07-18 17:10:36 +0300

    wip

--- app/jmetrix_web.py
+++ app/jmetrix_web.py
@@ -1,18 +1,24 @@
-import flask
+#!/usr/bin/env python3
+from flask import Flask, request, render_template
+import subprocess
+import urllib.parse
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/jmetrix', methods=['GET'])
-def jmetrix():
+@app.route('/daily_rpt', methods=['GET'])
+def run_script():
     url = request.args.get('url')
     token = request.args.get('token')
     project = request.args.get('project')
-    verbose = request.args.get('verbose', 'False')
 
-    result = subprocess.run(['./jmetrix.py', 'daily_rpt', url, token, project, verbose], capture_output=True)
+    # Run the Python script using subprocess
+    command = f'/mnt/c/Users/i.dimitrakopoulos/PycharmProjects/jmetrix/app/jmetrix.py daily_rpt -u "{url}" -t "{token}" -p "{project}" -V'
 
-    return result.stdout
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    output = result.stdout
+
+    return render_template('output.html', output=output)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000,  debug=True)
