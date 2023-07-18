ffd4cd60d36d1429e16364b9c8f1871012e5aba0
idimitrakopoulos <idimitrakopoulos@gmail.com>
2023-07-18 13:44:55 +0300

    wip

---
+++ app/jmetrix_web.py
@@ -0,0 +1,17 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/jmetrix', methods=['GET'])
+def jmetrix():
+    url = request.args.get('url')
+    token = request.args.get('token')
+    project = request.args.get('project')
+    verbose = request.args.get('verbose', 'False')
+
+    result = subprocess.run(['./jmetrix.py', 'daily_rpt', url, token, project, verbose], capture_output=True)
+
+    return result.stdout
+
+if __name__ == '__main__':
+    app.run(debug=True)
