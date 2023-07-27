a9283b3e43b76801dec54ab66a9b98c4a9b979dc
Alexandros Valarakos <avalarakos@lnw.com>
2023-07-27 10:51:09 +0300

    example of posting canned msg to slack

---
+++ app/examples/slack_integration/slack_bot_example_post_msg.py
@@ -0,0 +1,31 @@
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+logging.basicConfig(level=logging.INFO)
+
+# slack_token=os.environ["SLACK_BOT_TOKEN"]
+slack_token = "xoxb-2936559976-5583082766166-19beZsqgNZ6efQWst0BHUhEn"
+
+client = WebClient(token=slack_token)
+
+
+def post_to_slack(slack_message, slack_channel):
+    try:
+        client.chat_postMessage(
+            channel=slack_channel,
+            text=slack_message
+        )
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["error"]
+
+
+def main():
+    # https://app.slack.com/client/T02TJGFUQ/C05HE4M4VPE?cdn_fallback=2
+    slack_channel_id = "C05HE4M4VPE"
+    post_to_slack("Hello from your app! :tada:", slack_channel_id)
+
+
+if __name__ == "__main__":
+    main()
