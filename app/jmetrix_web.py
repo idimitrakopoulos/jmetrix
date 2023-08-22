cb138495352887481d71ae51816767b05358caa3
Ben Holford <>
2023-08-22 13:12:22 +0100

    pass command and args as a list

--- app/jmetrix_web.py
+++ app/jmetrix_web.py
@@ -4,7 +4,6 @@ import subprocess
 import os
 import logging
 import util.logger as logger
-import shlex
 
 log = logger.setup_custom_logger('root', logging.DEBUG)
 app = Flask(__name__)
@@ -15,45 +14,40 @@ def health():
 
 @app.route('/daily_rpt', methods=['GET'])
 def run_daily_rpt():
-    url = shlex.quote(request.args.get('url'))
-    token = shlex.quote(request.args.get('token'))
-    project = shlex.quote(request.args.get('project'))
-    discovery = shlex.quote(request.args.get('discovery'))
+    url = request.args.get('url')
+    token = request.args.get('token')
+    project = request.args.get('project')
+    discovery = request.args.get('discovery')
 
-    # Run the Python script using subprocess
+    command = [f'{os.getcwd()}/jmetrix.py', 'daily_rpt', '-u', url, '-t', token, '-p', project, '-V']
     if discovery is not None:
-        command = f'{os.getcwd()}/jmetrix.py daily_rpt -u "{url}" -t "{token}" -p "{project}" -V -d'
-    else:
-        command = f'{os.getcwd()}/jmetrix.py daily_rpt -u "{url}" -t "{token}" -p "{project}" -V'
+        command.append('-d')
 
-    log.debug(command)
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    log.debug(" ".join(command))
+    result = subprocess.run(command, capture_output=True, text=True)
     output = result.stdout + result.stderr
 
     return render_template('preformatted.html', output=output)
 
 @app.route('/swimlane_rpt', methods=['GET'])
 def run_swimlane_rpt():
-    url = shlex.quote(request.args.get('url'))
-    token = shlex.quote(request.args.get('token'))
-    project = shlex.quote(request.args.get('project'))
-    date_from = shlex.quote(request.args.get('date_from'))
-    date_to = shlex.quote(request.args.get('date_to'))
-    jira_label = shlex.quote(request.args.get('jira_label'))
-    extra_jira_label = shlex.quote(request.args.get('extra_jira_label'))
+    url = request.args.get('url')
+    token = request.args.get('token')
+    project = request.args.get('project')
+    date_from = request.args.get('date_from')
+    date_to = request.args.get('date_to')
+    jira_label = request.args.get('jira_label')
+    extra_jira_label = request.args.get('extra_jira_label')
 
-    # Run the Python script using subprocess
+    command = [f'{os.getcwd()}/jmetrix.py', 'swimlane_rpt', '-u', url, '-t', token, '-p', project, '-f', date_from, '-T', date_to, '-l', jira_label, '-V']
     if extra_jira_label is not None:
-        command = f'{os.getcwd()}/jmetrix.py swimlane_rpt -u "{url}" -t "{token}" -p "{project}" -f "{date_from}" -T "{date_to}" -l "{jira_label}" -L "{extra_jira_label}" -V'
-    else:
-        command = f'{os.getcwd()}/jmetrix.py swimlane_rpt -u "{url}" -t "{token}" -p "{project}" -f "{date_from}" -T "{date_to}" -l "{jira_label}" -V'
+        command.extend(['-L', extra_jira_label])
 
-    log.debug(command)
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    log.debug(" ".join(command))
+    result = subprocess.run(command, capture_output=True, text=True)
     output = result.stdout + result.stderr
 
     return render_template('preformatted.html', output=output)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,  debug=True)
-
